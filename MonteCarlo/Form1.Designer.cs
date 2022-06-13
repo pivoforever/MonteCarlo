11b3e1e5473f24d99e6e1dd2c8543fae0cb22d74
Vitaliy <pivonever@gmail.com>
2022-06-13 14:38:40 +0500

    Разработал макет

--- MonteCarlo/Form1.Designer.cs
+++ MonteCarlo/Form1.Designer.cs
@@ -38,36 +38,45 @@ namespace MonteCarlo
             this.textBoxXmax = new System.Windows.Forms.TextBox();
             this.textBoxStep = new System.Windows.Forms.TextBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(620, 398);
+            this.button1.Location = new System.Drawing.Point(467, 503);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(168, 40);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Рассчитать";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBoxXmin
             // 
-            this.textBoxXmin.Location = new System.Drawing.Point(16, 414);
+            this.textBoxXmin.Location = new System.Drawing.Point(11, 523);
             this.textBoxXmin.Name = "textBoxXmin";
-            this.textBoxXmin.Size = new System.Drawing.Size(121, 20);
+            this.textBoxXmin.Size = new System.Drawing.Size(129, 20);
             this.textBoxXmin.TabIndex = 1;
             // 
             // textBoxXmax
             // 
-            this.textBoxXmax.Location = new System.Drawing.Point(156, 414);
+            this.textBoxXmax.Location = new System.Drawing.Point(151, 523);
             this.textBoxXmax.Name = "textBoxXmax";
-            this.textBoxXmax.Size = new System.Drawing.Size(121, 20);
+            this.textBoxXmax.Size = new System.Drawing.Size(129, 20);
             this.textBoxXmax.TabIndex = 2;
             // 
             // textBoxStep
             // 
-            this.textBoxStep.Location = new System.Drawing.Point(301, 414);
+            this.textBoxStep.Location = new System.Drawing.Point(296, 523);
             this.textBoxStep.Name = "textBoxStep";
             this.textBoxStep.Size = new System.Drawing.Size(121, 20);
             this.textBoxStep.TabIndex = 3;
@@ -90,15 +99,113 @@ namespace MonteCarlo
             series4.Name = "Series2";
             this.chart1.Series.Add(series3);
             this.chart1.Series.Add(series4);
-            this.chart1.Size = new System.Drawing.Size(702, 361);
+            this.chart1.Size = new System.Drawing.Size(726, 439);
             this.chart1.TabIndex = 4;
             this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 475);
+            this.label1.MaximumSize = new System.Drawing.Size(150, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Введите нижний предел интегрирования";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 475);
+            this.label2.MaximumSize = new System.Drawing.Size(150, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Введите верхний предел интегрирования";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 475);
+            this.label3.MaximumSize = new System.Drawing.Size(150, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Введите количество точек";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(802, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Выберите функцию";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(810, 84);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(80, 17);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Функция 1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(810, 180);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 17);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Функция 2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(810, 284);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(80, 17);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Функция 3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(808, 407);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(807, 362);
+            this.label5.MaximumSize = new System.Drawing.Size(150, 30);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 26);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Результат рассчета интеграла";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1137, 555);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.textBoxStep);
             this.Controls.Add(this.textBoxXmax);
@@ -119,6 +226,15 @@ namespace MonteCarlo
         private System.Windows.Forms.TextBox textBoxXmax;
         private System.Windows.Forms.TextBox textBoxStep;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
